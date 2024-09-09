0936e722477ce285ff85a453c71f4b91da9e783c
itskoi <itskoiwork@gmail.com>
2024-09-09 03:44:19 -0700

    Add GROBID for PDF Parsing & fix minor bug in text_embeddings method

--- src/utils/retriever.py
+++ src/utils/retriever.py
@@ -29,7 +29,7 @@ class VectorDBRetriever(BaseRetriever):
 
     def _retrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
         """Retrieve."""
-        query_embedding = self._embed_model.get_text_embedding(query_bundle.query_str)
+        query_embedding = self._embed_model.get_query_embedding(query_bundle.query_str)
         vector_store_query = VectorStoreQuery(
             query_embedding=query_embedding,
             similarity_top_k=self._similarity_top_k,
