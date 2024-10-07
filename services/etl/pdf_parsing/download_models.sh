e138ebb284d3d10733870c5302ef119f4f20e861
Quang Doan <quangd@uniquify.com>
2024-10-07 01:55:34 -0700

    feat (etl): Detect layout & formulas in documents in preparation for PDF parsing.

---
+++ services/etl/pdf_parsing/download_models.sh
@@ -0,0 +1,4 @@
+cd models
+git lfs install
+git lfs clone https://huggingface.co/opendatalab/PDF-Extract-Kit/models/Layout
+git lfs clone https://huggingface.co/opendatalab/PDF-Extract-Kit/models/MFD
